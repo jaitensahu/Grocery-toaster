3a34413801a99a305835f9a5d3735b75a8b29b0d
Jaiten <Jaitensahu9@gmail.com>
2023-12-27 21:30:50 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import InputCard from './Component/InputCard';
 import {useState} from 'react'
 import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
+import { useEffect } from 'react';
 
 import 'react-toastify/dist/ReactToastify.css';
 let i=0;
@@ -18,17 +19,43 @@ function App() {
       position: toast.POSITION.TOP_CENTER
     });
   }
+
+useEffect(()=>{
+setDataArr([...JSON.parse(localStorage.getItem("data"))]);
+}, [])
+  
 let [dataArr, setDataArr]=useState([]);
 function updateArrFn(updatedArr){
+  setDataArr([...updatedArr]);
   console.log(updatedArr);
-  setDataArr([...updatedArr])
+  localStorage.setItem("data", JSON.stringify(updatedArr));
 }
+
 function deleteData(selectedIdx){
   let filteredArr=dataArr.filter((ele,idx)=>{
     return selectedIdx != idx;
   })
   setDataArr([...filteredArr])
+  localStorage.setItem("data", JSON.stringify(filteredArr));
 }
+
+function checkboxFn(id){
+  console.log(id);
+ let newArrayOncheck= dataArr.map((ele,idx)=>{
+    if(idx==id){
+      ele.checcked? ele.checcked=false: ele.checcked=true;
+    }
+    return ele;
+  })
+  setDataArr(newArrayOncheck)
+  localStorage.setItem("data", JSON.stringify(newArrayOncheck));
+  // console.log(newArrayOncheck);
+
+  
+}
+
+let [groceryName, setGroceryName]=useState();
+let [checked, setChekbox]=useState(true);
   return (
     <div className="App">
       <ToastContainer />
@@ -36,8 +63,8 @@ function deleteData(selectedIdx){
       {dataArr.map((data,idx)=>{
           return <div key={i++} className="item">
             <div className="input">
-            <input type="checkbox" />
-            <label htmlFor="">{data.grocery}</label>
+            <input type="checkbox" onChange={()=> checkboxFn(idx)} checked={data.checcked}/>
+            <label htmlFor="" style={{textDecoration:(data.checcked)? "line-through":"none"}}>{data.grocery}</label>
             </div>
             <button onClick={()=>{
                                   deleteData(idx)
--- src/Component/InputCard.js
+++ src/Component/InputCard.js
@@ -1,16 +1,19 @@
 import './InputCard.css'
 import {useState} from 'react'
+
 const InputCard=(props)=>{
     // console.log(props.dataArr);
     const [grocery, setInputData]=useState("");
     const [inp, setInp]=useState("")
     function addGrocery(){
+        let checcked=false;
         console.log(grocery);
         if(grocery==""){
             props.error("Please Provide Some Data");
         }else{
         let newData={
-            grocery
+            grocery,
+            checcked
         }
         props.dataArr.push(newData)
         props.notify("New Grocery Added");
@@ -27,7 +30,7 @@ const InputCard=(props)=>{
         <div className="inputCard">
             <h1>Grocery Bud</h1>
             <div className="inputContainer">
-             <input type="text" onChange={inputChange} value={inp}/>
+             <input type="text" onChange={inputChange} />
              <button onClick={addGrocery}>Add Item</button>
              </div>
         </div>
