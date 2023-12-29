d3aca3e1943b83ccc4aa7e2e8dc0073ef9b01770
Jaiten <Jaitensahu9@gmail.com>
2023-12-30 01:40:27 +0530

    UI updated

--- src/App.js
+++ src/App.js
@@ -1,80 +1,100 @@
-import './App.css';
-import InputCard from './Component/InputCard';
-import {useState} from 'react'
-import React from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import { useEffect } from 'react';
+import "./App.css";
+import InputCard from "./Component/InputCard";
+import { useState } from "react";
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { useEffect } from "react";
 
-import 'react-toastify/dist/ReactToastify.css';
-let i=0;
+import "react-toastify/dist/ReactToastify.css";
+let i = 0;
 function App() {
   const notify = (str) => {
     // toast("Deleted!!!");
     toast.success(str, {
-      position: toast.POSITION.TOP_CENTER
+      position: toast.POSITION.TOP_CENTER,
     });
-  }
-  const error=(str)=>{
+  };
+  const error = (str) => {
     toast.error(str, {
-      position: toast.POSITION.TOP_CENTER
+      position: toast.POSITION.TOP_CENTER,
     });
-  }
+  };
 
-useEffect(()=>{
-  if(localStorage.getItem("data")!=null){
-    setDataArr([...JSON.parse(localStorage.getItem("data"))]);
-  }
-}, [])
-  
-let [dataArr, setDataArr]=useState([]);
-function updateArrFn(updatedArr){
-  setDataArr([...updatedArr]);
-  console.log(updatedArr);
-  localStorage.setItem("data", JSON.stringify(updatedArr));
-}
-
-function deleteData(selectedIdx){
-  let filteredArr=dataArr.filter((ele,idx)=>{
-    return selectedIdx != idx;
-  })
-  setDataArr([...filteredArr])
-  localStorage.setItem("data", JSON.stringify(filteredArr));
-}
-
-function checkboxFn(id){
-  console.log(id);
- let newArrayOncheck= dataArr.map((ele,idx)=>{
-    if(idx==id){
-      ele.checcked? ele.checcked=false: ele.checcked=true;
+  useEffect(() => {
+    if (localStorage.getItem("data") != null) {
+      setDataArr([...JSON.parse(localStorage.getItem("data"))]);
     }
-    return ele;
-  })
-  setDataArr(newArrayOncheck)
-  localStorage.setItem("data", JSON.stringify(newArrayOncheck));
-  // console.log(newArrayOncheck);
+  }, []);
 
-  
-}
+  let [dataArr, setDataArr] = useState([]);
+  function updateArrFn(updatedArr) {
+    setDataArr([...updatedArr]);
+    console.log(updatedArr);
+    localStorage.setItem("data", JSON.stringify(updatedArr));
+  }
 
-let [groceryName, setGroceryName]=useState();
-let [checked, setChekbox]=useState(true);
+  function deleteData(selectedIdx) {
+    let filteredArr = dataArr.filter((ele, idx) => {
+      return selectedIdx != idx;
+    });
+    setDataArr([...filteredArr]);
+    localStorage.setItem("data", JSON.stringify(filteredArr));
+  }
+
+  function checkboxFn(id) {
+    console.log(id);
+    let newArrayOncheck = dataArr.map((ele, idx) => {
+      if (idx == id) {
+        ele.checcked ? (ele.checcked = false) : (ele.checcked = true);
+      }
+      return ele;
+    });
+    setDataArr(newArrayOncheck);
+    localStorage.setItem("data", JSON.stringify(newArrayOncheck));
+    // console.log(newArrayOncheck);
+  }
+
+  let [groceryName, setGroceryName] = useState();
+  let [checked, setChekbox] = useState(true);
   return (
     <div className="App">
       <ToastContainer />
-      <InputCard cardArr={updateArrFn} dataArr={dataArr} notify={notify} error={error}/>
-      {dataArr.map((data,idx)=>{
-          return <div key={i++} className="item">
+      <InputCard
+        cardArr={updateArrFn}
+        dataArr={dataArr}
+        notify={notify}
+        error={error}
+      />
+      {dataArr.map((data, idx) => {
+        return (
+          <div key={i++} className="item">
             <div className="input">
-            <input type="checkbox" onChange={()=> checkboxFn(idx)} checked={data.checcked}/>
-            <label htmlFor="" style={{textDecoration:(data.checcked)? "line-through":"none"}}>{data.grocery}</label>
+              <input
+                type="checkbox"
+                onChange={() => checkboxFn(idx)}
+                checked={data.checcked}
+              />
+              <label
+                htmlFor=""
+                style={{
+                  textDecoration: data.checcked ? "line-through" : "none",
+                }}
+              >
+                {data.grocery}
+              </label>
             </div>
-            <button onClick={()=>{
-                                  deleteData(idx)
-                                  notify("Grocery Deleted"); 
-              }}>Delete</button>
+            <button class="button" onClick={() => {
+                deleteData(idx);
+                notify("Grocery Deleted");
+              }}>
+              <svg viewBox="0 0 448 512" class="svgIcon">
+                <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
+              </svg>
+            </button>
+            
           </div>
-      })
-      }
+        );
+      })}
     </div>
   );
 }
--- src/Component/InputCard.js
+++ src/Component/InputCard.js
@@ -1,40 +1,52 @@
-import './InputCard.css'
-import {useState} from 'react'
+import "./InputCard.css";
+import { useState } from "react";
 
-const InputCard=(props)=>{
-    // console.log(props.dataArr);
-    const [grocery, setInputData]=useState("");
-    const [inp, setInp]=useState("")
-    function addGrocery(){
-        let checcked=false;
-        console.log(grocery);
-        if(grocery==""){
-            props.error("Please Provide Some Data");
-        }else{
-        let newData={
-            grocery,
-            checcked
-        }
-        props.dataArr.push(newData)
-        props.notify("New Grocery Added");
-        props.cardArr(props.dataArr);
+const InputCard = (props) => {
+  // console.log(props.dataArr);
+  const [grocery, setInputData] = useState("");
+  const [inp, setInp] = useState("");
+  function addGrocery() {
+    let checcked = false;
+    console.log(grocery);
+    if (grocery == "") {
+      props.error("Please Provide Some Data");
+    } else {
+      let newData = {
+        grocery,
+        checcked,
+      };
+      props.dataArr.push(newData);
+      props.notify("New Grocery Added");
+      props.cardArr(props.dataArr);
     }
+  }
+  function inputChange(e) {
+    setInputData(e.target.value);
+    setInp(e.target.value);
+  }
+  return (
+    <div className="inputCard">
+      <h1>Grocery Bud</h1>
+      <div className="inputContainer">
+        <input type="text" onChange={inputChange} placeholder="Enter Grocery name"/>
+        <button class="cssbuttons-io-button" onClick={addGrocery}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+          >
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path
+              fill="currentColor"
+              d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"
+            ></path>
+          </svg>
+          <span>Add</span>
+        </button>
+      </div>
+    </div>
+  );
+};
 
-    }
-    function inputChange(e){
-        setInputData(e.target.value)
-        setInp(e.target.value)
-        
-    }
-    return(
-        <div className="inputCard">
-            <h1>Grocery Bud</h1>
-            <div className="inputContainer">
-             <input type="text" onChange={inputChange} />
-             <button onClick={addGrocery}>Add Item</button>
-             </div>
-        </div>
-    )
-}
-
-export default InputCard
+export default InputCard;
